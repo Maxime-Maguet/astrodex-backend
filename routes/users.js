8a8d7526731947630577bebf0dcc2746676b4d08
Allan <christoph.allan77120@gmail.com>
2026-03-02 14:28:53 +0100

    J1 after lunch

--- routes/users.js
+++ routes/users.js
@@ -5,23 +5,25 @@ const { checkBody } = require("../modules/checkBody");
 const uid2 = require("uid2");
 const bcrypt = require('bcrypt');
 
+
 //route Signup pour la premiere connexion du client
 
 router.post("/signup", (req, res) => {
-  if (!checkBody(req.body, ["username", "password"])) {
+  if (!checkBody(req.body, ["username", "password", "email"])) {
     res.json({ result: false, error: "Missing or empty fields" });
     return;
   }
 
   // trouver si l'utilisateur existe déjà en BDD sinon le créer
 
-  useStore.findOne({ username: req.body.username }).then(data => {
+  User.findOne({ username: req.body.username }).then(data => {
     if (data === null) {
 
       const hash = bcrypt.hashSync("password", 10);
 
       const newUser = new User({
         username: req.body.username,
+        email: req.body.email,
         password: hash,
         token: uid2(32),
       });

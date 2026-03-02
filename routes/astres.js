ee5cb71db8e519e1818c9fd9edb34cdb77531f35
Maxime-Maguet <maxime.maguet@gmail.com>
2026-03-02 14:26:13 +0100

    commit j1 after lunch

--- routes/astres.js
+++ routes/astres.js
@@ -2,4 +2,14 @@ var express = require("express");
 var router = express.Router();
 const Astre = require("../models/astres");
 
+router.get("/", (req, res) => {
+  Astre.find()
+    .then((data) => {
+      res.json({ result: true, astres: data });
+    })
+    .catch((error) => {
+      res.json({ result: false, error: error.message });
+    });
+});
+
 module.exports = router;

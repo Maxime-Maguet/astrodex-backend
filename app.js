1e7d164d8f21cbb9e60295b06ac5d834114a2e65
Maxime-Maguet <maxime.maguet@gmail.com>
2026-03-02 15:41:11 +0100

    routes delete wip

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ require("./models/connection");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var astresRouter = require("./routes/astres");
-<<<<<<< HEAD
-=======
-
->>>>>>> ee5cb71db8e519e1818c9fd9edb34cdb77531f35
 var app = express();
 const fileUpload = require("express-fileupload");
 app.use(fileUpload());
@@ -27,8 +23,4 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/astres", astresRouter);
-<<<<<<< HEAD
-=======
-
->>>>>>> ee5cb71db8e519e1818c9fd9edb34cdb77531f35
 module.exports = app;
--- routes/astres.js
+++ routes/astres.js
@@ -3,23 +3,24 @@ var router = express.Router();
 const Astre = require("../models/astres");
 const User = require("../models/users");
 
-
-router.put('/capturer',(req, res) =>{
-User.findOne ({ token: req.params.token }).then(user => {
-        if (!user) {
-            return res.json({ result: false, error: 'User not found' });
-        }
-        if(user.discoversAstres.includes(req.body.astreId)){
-            res.json({result :false, message :"Astre déjà capturé !" })
-        }else{
-
-            user.discoversAstres.push(req.body.astreId);
-             user.save().then(() => {
-                            res.json({ result: true, message: "Super ! l'astre a été capturé" });
-             }) 
-        }
-            });
+router.put("/capturer", (req, res) => {
+  User.findById(req.body.userId).then((user) => {
+    if (!user) {
+      return res.json({ result: false, error: "User not found" });
+    }
+    if (user.discoversAstres.includes(req.body.astreId)) {
+      res.json({ result: false, message: "Astre déjà capturé !" });
+    } else {
+      user.discoversAstres.push(req.body.astreId);
+      user.save().then((user) => {
+        user.populate("discoversAstres").then((userAstres) => {
+          res.json({ result: true, user: userAstres });
         });
+      });
+    }
+  });
+});
+
 router.get("/", (req, res) => {
   Astre.find()
     .then((data) => {
@@ -31,5 +32,3 @@ router.get("/", (req, res) => {
 });
 
 module.exports = router;
-//id user et id planete avec find one et update res.json (astres capturé), par rapport a id de l'astre si discover include id de l'adstre
-// astre deja capturé si id trouvé, utiliser findbyid

04c316a3458a146070d1d2d4bd2cf733a988fcc8
Allan <christoph.allan77120@gmail.com>
2026-03-02 10:51:22 +0100

    Route signup

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,44 @@
 var express = require("express");
 var router = express.Router();
 const User = require("../models/users");
+const { checkBody } = require("../modules/checkBody");
+const { hash } = require("bcrypt");
+const uid2 = require("uid2");
+
+//route Signup pour la premiere connexion du client
+
+router.post("/signup", (req, res) => {
+  if (!checkBody(req.body, ["username", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+
+  // trouver si l'utilisateur existe déjà en BDD sinon le créer
+
+  useStore.findOne({ username: req.body.username }).then(data => {
+    if (data === null) {
+      const hash = bcrypt.hashSync("password", 10);
+
+      const newUser = new User({
+        username: req.body.username,
+        password: hash,
+        token: uid2(32),
+      });
 
 
+// création de l'utilisateur avec le .save()
 
+      newUser.save().then(newDoc => {
+        res.json({ result: true, token: newDoc.token });
+      });
+    } else {
+
+
+// Faux si utilisateur déjà crée 
+res.json({result: false, error: 'Users already Exists'});
+
+}
+  });
+});
 
 module.exports = router;

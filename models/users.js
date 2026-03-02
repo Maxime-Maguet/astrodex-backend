aec466136b9d3edc9b0638cb9432dcf24a0a7563
Your name <Your email>
2026-03-02 10:38:59 +0100

    New commit

--- models/users.js
+++ models/users.js
@@ -1,6 +1,16 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({});
+const userSchema = mongoose.Schema({
+username : String,
+email : String,
+password: String,
+token : String,
+xp : Number,
+bcrypt : Number,
+equipement : String,
+discoversAstres : {type: mongoose.Schema.Types.ObjectId, ref : "astres"}
+
+});
 
 const User = mongoose.model("users", userSchema);
 
